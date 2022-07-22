01f6812d3f3a4aa48df610110c6cf6b85d61b772
adnKsharp <darkmark404@protonmail.com>
2022-07-22 10:18:22 -0600

    First...

---
+++ imgReader/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace imgReader
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(33)))));
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Margin = new System.Windows.Forms.Padding(4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(110, 28);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Input);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(526, 260);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.Go);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Run);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Location = new System.Drawing.Point(13, 49);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(534, 267);
+			this.panel1.TabIndex = 2;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(33)))));
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(459, 13);
+			this.button2.Margin = new System.Windows.Forms.Padding(4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(88, 28);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Mode);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(27)))), ((int)(((byte)(33)))));
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Location = new System.Drawing.Point(363, 13);
+			this.button3.Margin = new System.Windows.Forms.Padding(4);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(88, 28);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Rotate);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+			this.ClientSize = new System.Drawing.Size(560, 332);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("SF Pro Display", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.White;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Setup);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button2;
+		public System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
---
+++ imgReader/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace imgReader
+{
+	public partial class Form1 : Form
+	{
+		int[] y = { 0, 0 };
+		bool zoom, hide, mode, move = true;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Setup(object sender, EventArgs e)
+		{
+			Text = "Visualizador de imagenes";
+			button1.Text = "Abrir imagen";
+			button2.Text = "Mover";
+			button3.Text = "Girar";
+			button3.Hide();
+		}
+
+		private void Mode(object sender, EventArgs e)
+		{
+			mode = !mode;
+			zoom = false;
+			move = false;
+			if (mode)
+			{
+				button2.Text = "Ampliar";
+				zoom = true;
+			}
+			else
+			{
+				button2.Text = "Mover";
+				move = true;
+			}
+		}
+
+		private void Rotate(object sender, EventArgs e)
+		{
+			Image rotate = pictureBox1.Image;
+			if (zoom)
+				rotate.RotateFlip(RotateFlipType.Rotate90FlipNone);
+			else
+				rotate.RotateFlip(RotateFlipType.Rotate270FlipNone);
+			pictureBox1.Image = rotate;
+		}
+
+		private void Input(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				button3.Show();
+				try
+				{
+					pictureBox1.Load(openFileDialog1.FileName);
+					pictureBox1.Width = 525;
+					pictureBox1.Height = 260;
+					pictureBox1.Location = new Point(3, 3);
+				}
+				catch
+				{
+					button3.Hide();
+				}
+			}
+		}
+
+		private void Go(object sender, EventArgs e)
+		{
+			hide = !hide;
+			if (hide)
+				Cursor.Hide();
+			else
+				Cursor.Show();
+			y[0] = Cursor.Position.X;
+			y[1] = Cursor.Position.Y;
+		}
+
+		private void Run(object sender, MouseEventArgs e)
+		{
+			if (hide)
+			{
+				int[] x = { Cursor.Position.X, Cursor.Position.Y };
+				if (zoom)
+				{
+					pictureBox1.Width -= (x[0] - y[0]);
+					pictureBox1.Height -= (x[1] - y[1]);
+					pictureBox1.Location = new Point(pictureBox1.Location.X + (x[0] - y[0]), pictureBox1.Location.Y);
+				}
+				else if (move)
+				{
+					pictureBox1.Location = new Point(pictureBox1.Location.X + (x[0] - y[0]), pictureBox1.Location.Y + (x[1] - y[1]));
+				}
+				Cursor.Position = new Point(y[0], y[1]);
+			}
+		}
+	}
+}
